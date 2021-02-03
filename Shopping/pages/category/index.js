10664957c12c3845fb2358ff5607a89acc768a27
powerful-chen <2930763626@qq.com>
2021-02-03 11:43:52 +0800

    添加分类导航页

--- Shopping/pages/category/index.js
+++ Shopping/pages/category/index.js
@@ -1,66 +1,39 @@
 // pages/category/index.js
+import { request } from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 左侧的菜单数据
+    leftMenuList:[],
+    // 右侧的商品数据
+    rightContent:[],
+    // 被点击的左侧的菜单
+    currentIndex:0
   },
+  // 接口的返回数据
+  Cates:[],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取分类数据
+  getCates(){
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    })
+    .then(res=>{
+      this.Cates=res.data.message;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      // 构造左侧的大菜单数据
+      let leftMenuList=this.Cates.map(v=>v.cat_name);
+      let rightContent=this.Cates[0].children;
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
   }
 })
--- Shopping/pages/index/index.js
+++ Shopping/pages/index/index.js
@@ -1,8 +1,6 @@
 //Page Object
 // 引入 用来发送请求的 方法 一定要把路径补全
-import {
-  request
-} from "../../request/index.js"
+import { request } from "../../request/index.js"
 Page({
   data: {
     // 轮播图数组
